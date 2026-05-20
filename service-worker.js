ae35dde8819f6d640295f4baf89c4ba0db29ac57
Mubarak1980 <101741909+Mubarak1980@users.noreply.github.com>
2026-05-20 18:26:41 +0300

    Update service worker to version 12

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER (CHROME STABLE FINAL)
 // =========================
 
-const CACHE_NAME = "mission-cache-v11";
+const CACHE_NAME = "mission-cache-v12";
 
 // ✅ FIX: lock base path (do NOT use scope dynamically)
 const BASE = "/Mission-app/";
