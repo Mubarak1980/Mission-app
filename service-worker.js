6368e634cc73bccf646e7f26cd88e64f7396fb63
Mubarak1980 <101741909+Mubarak1980@users.noreply.github.com>
2026-05-20 14:17:05 +0300

    Update service worker cache version to v217

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER (PRODUCTION SAFE)
 // =========================
 
-const CACHE_NAME = "mission-cache-v216";
+const CACHE_NAME = "mission-cache-v217";
 
 // Use scope correctly
 const BASE = self.registration.scope;
