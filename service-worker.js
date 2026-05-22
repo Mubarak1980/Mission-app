a8a7a7553225300ebe4218b0529fe1f693fdb457
Mubarak1980 <101741909+Mubarak1980@users.noreply.github.com>
2026-05-22 10:41:30 +0300

    Update service worker cache version to v26

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
 const BASE_PATH = self.registration?.scope || "./";
 
 // Increment this version string whenever you change files
-const CACHE_NAME = "mission-cache-v25";
+const CACHE_NAME = "mission-cache-v26";
 
 // ==========================================
 // STATIC APP SHELL
