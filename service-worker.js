d62a370229d9cf0ebdaed9c4eec08f1aa9854fae
Mubarak1980 <101741909+Mubarak1980@users.noreply.github.com>
2026-05-23 10:00:14 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // SERVICE WORKER (ULTRA-STABLE PWA ENGINE V42)
 // ==========================================
 
-const CACHE_NAME = "mission-cache-v44";
+const CACHE_NAME = "mission-cache-v45";
 
 const APP_SHELL = [
   "index.html",
