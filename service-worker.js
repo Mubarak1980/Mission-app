5125515092edfdd259a05d8d149cf3f2bd8d9591
Mubarak1980 <101741909+Mubarak1980@users.noreply.github.com>
2026-05-25 16:55:35 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // SERVICE WORKER (ULTRA-RESILIENT CHROME PRODUCTION ENGINE V65)
 // ==========================================================
 
-const CACHE_NAME = "mission-cache-v9";
+const CACHE_NAME = "mission-cache-v10";
 
 const APP_SHELL = [
   "/Mission-app/",
