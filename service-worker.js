dc5c1c0c7c1f8d027110961e927c0b0723130ef3
Mubarak1980 <101741909+Mubarak1980@users.noreply.github.com>
2026-06-02 08:16:23 +0300

    Update service worker cache version to v5

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // 🚀 MISSION APP PWA SERVICE WORKER (v69)
 // ==========================================================
 
-const CACHE_NAME = "mission-cache-v4";
+const CACHE_NAME = "mission-cache-v5";
 const BASE_URL = new URL("./", self.location.href).toString();
 
 const APP_SHELL = [
