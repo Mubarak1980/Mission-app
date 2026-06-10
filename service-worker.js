9fec1e8488fd8adc371c313b8fcaa03fa04fd3a3
Mubarak1980 <101741909+Mubarak1980@users.noreply.github.com>
2026-06-10 06:07:16 +0300

    Update service worker to version 79

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
 OFFLINE-FIRST + ROBUST INSTALL + SAFE NAVIGATION
 ========================================================== */
 
-const CACHE_NAME = "mission-v78";
+const CACHE_NAME = "mission-v79";
 
 const APP_SHELL = [
     "./",
