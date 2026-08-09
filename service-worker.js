b0d46c6b77f7052ef03a08174fa47cc4a8f1eaa9
Mubarak1980 <101741909+Mubarak1980@users.noreply.github.com>
2026-08-09 08:36:41 +0300

    Update service worker to version 2.0

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
 OFFLINE-FIRST + ROBUST INSTALL + SAFE NAVIGATION
 ========================================================== */
 
-const CACHE_NAME = "mission-v119";
+const CACHE_NAME = "mission-v120";
 
 const APP_SHELL = [
     "./",
